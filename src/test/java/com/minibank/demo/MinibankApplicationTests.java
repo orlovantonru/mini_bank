d7af4b7022d97c2435963c7759b1156b3d9a183f
orlovantonru <orlov_anton@mail.ru>
2023-08-10 15:29:01 +0300

    initial commit

---
+++ src/test/java/com/minibank/demo/MinibankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minibank.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
